6db4d31214de10669d2644c19636d0d8a0c74f23
Ken Zhu <k-zhu@onu.edu>
2022-11-16 00:55:09 -0500

    code finds locations of errors, not sure how to implement correction yet

--- Source.cpp
+++ Source.cpp
@@ -4,10 +4,12 @@
 
 using namespace std;
 
-vector<vector<int>> matrixMultiply(vector<int> m1, vector<int> m2);
 int vectorMultiplyAlphas(vector<int> alphas, vector<int> X, int symbol);
 int alphaAdd(int alpha1, int alpha2);
 int alphaMultiply(int alpha1, int alpha2);
+int alphaExponent(int alpha, int exponent);
+vector<vector<int>> inverseMatrix(vector<vector<int>> m);
+vector<int> findBetas(int sig1, int sig2);
 
 vector<vector<int>> addTable =
 {   {15, 4,  8,  14, 1,  10, 13, 9,  2,  7,  5,  12, 11, 6,  3,  0},
@@ -30,11 +32,17 @@ vector<vector<int>> addTable =
 
 int main() {
 	vector<int> message = {};
-	cout << "Enter your 11 symbol message: " << endl;
+	cout << "Enter your 11 symbol message RS(15, 11): " << endl;
+	cout << "The symbols should be between 0 which represents a0 and 15 which is true 0\n";
 	cout << "Note: the symbols will be processed in the same order you enter them\n";
 	while (message.size() < 11) {
 		int temp;
-		cin >> temp;
+		do {
+			cin >> temp;
+			if (temp < 0 || temp > 15) {
+				cout << "That is not a valid input, try again.\n";
+			}
+		} while (temp < 0 || temp > 15);
 		message.push_back(temp);
 	}
 
@@ -76,40 +84,196 @@ int main() {
 
 	// Validate U(x) for each of the 4 roots
 	vector<int> X = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-	cout << "Testing alpha-1: " << vectorMultiplyAlphas(codeword, X, 1) << endl;
-	cout << "Testing alpha-2: " << vectorMultiplyAlphas(codeword, X, 2) << endl;
-	cout << "Testing alpha-3: " << vectorMultiplyAlphas(codeword, X, 3) << endl;
-	cout << "Testing alpha-4: " << vectorMultiplyAlphas(codeword, X, 4) << endl << endl;
 
-	cout << "";
+	// Debugging print statements
+	// cout << "Testing alpha-1: " << vectorMultiplyAlphas(codeword, X, 1) << endl;
+	// cout << "Testing alpha-2: " << vectorMultiplyAlphas(codeword, X, 2) << endl;
+	// cout << "Testing alpha-3: " << vectorMultiplyAlphas(codeword, X, 3) << endl;
+	// cout << "Testing alpha-4: " << vectorMultiplyAlphas(codeword, X, 4) << endl << endl;
+
+	int check1 = vectorMultiplyAlphas(codeword, X, 1);
+	int check2 = vectorMultiplyAlphas(codeword, X, 2);
+	int check3 = vectorMultiplyAlphas(codeword, X, 3);
+	int check4 = vectorMultiplyAlphas(codeword, X, 4);
+	if (check1 == 15 && check2 == 15 && check3 == 15 && check4 == 15) {
+		cout << "All Clear! You have a valid codeword U(x)." << endl;
+	}
+	else {
+		return 1;
+	}
+
+	// Enter the location and symbols of error message e(x)
+	int errorX1, errorX2, errorA1, errorA2;
+	
+	do {
+		cout << "Enter the X-location of error symbol 1 (between 0 and 14): ";
+		cin >> errorX1;
+	} while (errorX1 < 0 || errorX1 > 14);
+
+	do {
+		cout << "What error symbol to use (between 0 and 14): ";
+		cin >> errorA1;
+	} while (errorA1 < 0 || errorA1 > 14);
+
+
+	switch (errorX1) {
+	case 0: 
+		errorX2 = 1;
+		cout << "The X-location of error symbol 2 is set to X1 by default.\n"; 
+		break;
+
+	case 14: 
+		errorX2 = 13;
+		cout << "The X-location of error symbol 2 is set to X13 by default.\n"; 
+		break;
+
+	default: 
+		do {
+			cout << "Select either " << errorX1 - 1 << " or " << errorX1 + 1
+				<< " to be the X-location of error symbol 2: ";
+			cin >> errorX2;
+	} while (errorX2 != errorX1 - 1 && errorX2 != errorX1 + 1);
+	break;
+	}
+
+	do {
+		cout << "What error symbol to use (between 0 and 14): ";
+		cin >> errorA2;
+	} while (errorA2 < 0 || errorA2 > 14);
+
+	// creation of R(x) which is U(x) + e(x)
+	vector<int> errorCode = codeword;
+	errorCode[errorX1] = alphaAdd(errorA1, codeword[errorX1]);
+	errorCode[errorX2] = alphaAdd(errorA2, codeword[errorX2]);
+
+	// Calculation of error syndromes 1 through 4 using R(x)
+	int syn1, syn2, syn3, syn4;
+	syn1 = vectorMultiplyAlphas(errorCode, X, 1);
+	syn2 = vectorMultiplyAlphas(errorCode, X, 2);
+	syn3 = vectorMultiplyAlphas(errorCode, X, 3);
+	syn4 = vectorMultiplyAlphas(errorCode, X, 4);
+
+	// Debug and print out our syndrome values
+	cout << "Syndrome values: \n";
+	cout << "S1 = " << syn1 << endl;
+	cout << "S2 = " << syn2 << endl;
+	cout << "S3 = " << syn3 << endl;
+	cout << "S4 = " << syn4 << endl;
+
+	// Use syndromes to find location of errors
+	vector<vector<int>> matrix = { {syn1, syn2},
+		                           {syn2, syn3} };
+	
+	// Inverse matrix to use for Sigma1 Sigma2 equation
+	vector<vector<int>> inverse = inverseMatrix(matrix);
+
+	cout << "Inverse Matrix: " << endl;
+	cout << inverse[0][0] << " " << inverse[0][1] << endl;
+	cout << inverse[1][0] << " " << inverse[1][1] << endl;
+
+	int sig1 = alphaAdd(alphaMultiply(inverse[1][0], syn3), alphaMultiply(inverse[1][1], syn4));
+	int sig2 = alphaAdd(alphaMultiply(inverse[0][0], syn3), alphaMultiply(inverse[0][1], syn4));
+
+	cout << "Sig1: " << sig1 << " Sig2: " << sig2 << endl;
+
+	vector<int> betas = findBetas(sig1, sig2);
+
+	if (betas[0] != 0) {
+		betas[0] *= -1;
+		betas[0] += 15;
+	}
+	betas[1] *= -1;
+	betas[1] += 15;
+
+	// Exit program if both error locations are in parity bits
+	if (betas[0] < 4 && betas[1] < 4) {
+		cout << "Both errors occurred in parity bits, we do not care.";
+		return 0;
+	}
+
+	// Print Error Locations
+	cout << "Error Locations: " << endl;
+	cout << "location 1: a" << betas[0] << " location 2: a" << betas[1] << endl;
 
 	return 0;
 }
 
+vector<vector<int>> inverseMatrix(vector<vector<int>> m) {
+	cout << "Matrix A: " << endl;
+	cout << m[0][0] << " " << m[0][1] << endl;
+	cout << m[1][0] << " " << m[1][1] << endl;
 
-vector<vector<int>> matrixMultiply(vector<int> m1, vector<int> m2)
-{
-	return vector<vector<int>>();
+	int determinant = alphaAdd(alphaMultiply(m[0][0], m[1][1]), alphaMultiply(m[0][1], m[1][0]));
+
+	cout << "Determinant: " << determinant << endl;
+
+	vector<vector<int>> cofactor = { {m[1][1], m[1][0]},
+		                             {m[0][1], m[0][0]} };
+
+	cout << "Cofactor: " << endl;
+	cout << cofactor[0][0] << " " << cofactor[0][1] << endl;
+	cout << cofactor[1][0] << " " << cofactor[1][1] << endl;
+
+	vector<vector<int>> inverse = { {15, 15},
+		                            {15, 15} };
+	determinant *= -1;
+	if (determinant < 0) determinant += 15;
+
+	inverse[0][0] = alphaMultiply(cofactor[0][0], determinant);
+	inverse[0][1] = alphaMultiply(cofactor[0][1], determinant);
+	inverse[1][0] = alphaMultiply(cofactor[1][0], determinant);
+	inverse[1][1] = alphaMultiply(cofactor[1][1], determinant);
+
+	return inverse;
 }
+
+vector<int> findBetas(int sig1, int sig2) {
+
+	vector<int> alphas = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+	vector<int> betas = {};
+
+	for (int i = 0; i < alphas.size(); i++) {
+		int x = 0, y = 0, z = 0;
+		// set y to sig1x1
+		y = alphaMultiply(sig1, alphaExponent(alphas[i], 1));
+		// set z to sig2x2
+		z = alphaMultiply(sig2, alphaExponent(alphas[i], 2));
+
+		// Debugging print statements
+		// cout << "Sig(a" << alphas[i] << "): ";
+		// cout << alphaAdd(alphaAdd(x, y), z) << endl;
+
+		if (alphaAdd(alphaAdd(x, y), z) == 15) {
+			betas.push_back(alphas[i]);
+			if (betas.size() == 2) break;
+		}
+	}
+
+	return betas;
+}
+
 
 int vectorMultiplyAlphas(vector<int> alphas, vector<int> X, int symbol)
 {
 	int sum = 15;
-	if (alphas.size() != X.size()) exit(1);
 	for (int i = 0; i < alphas.size(); i++) {
-		sum = alphaAdd(sum, (alphaMultiply(alphaMultiply(X[i], symbol), alphas[i])));
+		sum = alphaAdd(sum, alphaMultiply(alphas[i], (alphaExponent(symbol, X[i]))));
 	}
 	return sum;
 }
 
-int alphaAdd(int alpha1, int alpha2)
-{
+int alphaAdd(int alpha1, int alpha2) {
 	return addTable[alpha1][alpha2];
 }
 
-int alphaMultiply(int alpha1, int alpha2)
-{
+int alphaMultiply(int alpha1, int alpha2) {
 	if (alpha1 == 15) return 15;
 	else if (alpha2 == 15) return 15;
 	else return ((alpha1 + alpha2) % 15);
+}
+
+int alphaExponent(int alpha, int exponent) {
+	if (alpha == 0) return 15;
+	else if (exponent == 0) return 0;
+	else return (alpha * exponent) % 15;
 }
